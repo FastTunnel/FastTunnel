458b489fb2c3a156855e0e6739c393373d9cc694
SpringHgui <740360381@qq.com>
2019-12-26 19:56:13 +0800

    自动压缩

--- publish.cmd
+++ publish.cmd
@@ -1,9 +1,11 @@
 @echo off
 
 for /d %%p in (FastTunnel.Client,FastTunnel.Server) do (
-  CD ./%%p & for  %%I in (win-x64,osx-x64,linux-x64) do (
-	dotnet publish -o=../publish/%%p.%%I -c=release -f=netcoreapp3.1 -r=%%I --self-contained
+  CD ./%%p 
+  for  %%I in (win-x64,osx-x64,linux-x64) do (
+	dotnet publish -o=../publish/%%p.%%I -c=release -f=netcoreapp3.1 -r=%%I --self-contained & 7z a -tzip ../publish/%%p.%%I.zip ../publish/%%p.%%I
   )
+  cd ../
 )
 
 pause
