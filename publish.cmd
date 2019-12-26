e69df9665eb9a064d2f8acd92fbe5f0909376449
SpringHgui <740360381@qq.com>
2019-12-26 19:29:10 +0800

    添加发布脚本

---
+++ publish.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+for /d %%p in (FastTunnel.Client,FastTunnel.Server) do (
+  CD ./%%p & for  %%I in (win-x64,osx-x64,linux-x64) do (
+	dotnet publish -o=../publish/%%p.%%I -c=release -f=netcoreapp3.1 -r=%%I --self-contained
+  )
+)
+
+pause
