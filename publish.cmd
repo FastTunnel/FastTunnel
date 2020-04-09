e1182243aae222a15e52caf3cb85fe23d933b82e
SpringHgui <740360381@qq.com>
2020-04-09 14:34:14 +0800

    修改程序发布方式

--- publish.cmd
+++ publish.cmd
@@ -1,9 +1,10 @@
 @echo off
 
-for /d %%p in (FastTunnel.Client,FastTunnel.Server,SuiDao.Client) do (
+for /d %%p in (FastTunnel.Client,FastTunnel.Server,SuiDao.Client,SuiDao.Server) do (
   CD ./%%p 
+ 
   for  %%I in (win-x64,osx-x64,linux-x64) do (
-	dotnet publish -o=../publish/%%p.%%I -c=release -r=%%I -f=netcoreapp3.1  & 7z a -tzip ../publish/%%p.%%I.zip ../publish/%%p.%%I
+	dotnet publish -o=../publish/%%p.%%I -c=release -r=%%I /p:PublishSingleFile=true /p:PublishTrimmed=true & echo f |xcopy %~dp0%%p\appsettings.json %~dp0publish\%%p.%%I\appsettings.json & 7z a -tzip ../publish/%%p.%%I.zip ../publish/%%p.%%I
   )
   cd ../
 )
