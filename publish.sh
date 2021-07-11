2e8c3863a5dee8059632f0e486523074fc549c50
SpringHgui <740360381@qq.com>
2021-07-11 11:55:44 +0800

    添加发布脚本

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+rm -rf publish/*
+projects=("FastTunnel.Client" "FastTunnel.Server")
+plates=("win-x64" "win-arm" "osx-x64" "linux-arm" "linux-x64")
+for project in ${projects[*]}; do
+    echo
+    echo "=========开始发布：${project} ========="
+    echo
+    for plate in ${plates[*]}; do
+        echo "plate=${plate}"
+        echo src/$project/$project.csproj
+        dotnet publish $project/$project.csproj -o=publish/$project.$plate -c=release #-p:PublishTrimmed=true --nologo
+        echo
+        echo "=========开始打包 ========="
+        echo
+        cd publish && tar -zcvf $project.$plate.tar.gz $project.$plate
+        cd ../
+        # rm -rf publish/$project.$plate
+    done
+done
