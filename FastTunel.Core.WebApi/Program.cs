da2af5308cfaf5c73c9c7fb3aa4bdf8873a25db8
SpringHgui <740360381@qq.com>
2021-07-13 00:13:00 +0800

    增加api接口

---
+++ FastTunel.Core.WebApi/Program.cs
@@ -0,0 +1,9 @@
+namespace FastTunel.Core.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
--- FastTunnel.Server/Startup.cs
+++ FastTunnel.Server/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace FastTunnel.Server
 {
@@ -26,7 +27,11 @@ namespace FastTunnel.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FastTunel.Core.WebApi", Version = "v1" });
+            });
 
             // ------------------------Custom Business------------------------------
             services.AddTransient<IAuthenticationFilter, TestAuthenticationFilter>();
@@ -38,6 +43,8 @@ namespace FastTunnel.Server
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FastTunel.Core.WebApi v1"));
             }
 
             app.UseHttpsRedirection();
@@ -48,9 +55,7 @@ namespace FastTunnel.Server
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
         }
     }
