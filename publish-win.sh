752bc90f256c7449351ecf4c29888d792bcbd06b
Gui.H <740360381@qq.com>
2021-08-10 21:55:29 +0800

    Update publish-win.sh

--- publish-win.sh
+++ publish-win.sh
@@ -9,11 +9,11 @@ for project in ${projects[*]}; do
     for plate in ${plates[*]}; do
         echo "plate=${plate}"
         echo src/$project/$project.csproj
-        dotnet publish $project/$project.csproj -o=publish/$project.$plate -c=release #-p:PublishTrimmed=true --nologo
+        dotnet publish $project/$project.csproj -o=publish/$project.$plate -c=release || exit 1 #-p:PublishTrimmed=true --nologo
         echo
         echo "=========开始打包 ========="
         echo
-        cd publish && tar -zcvf $project.$plate.tar.gz $project.$plate
+        cd publish && tar -zcvf $project.$plate.tar.gz $project.$plate || exit 1 
         cd ../
         # rm -rf publish/$project.$plate
     done
