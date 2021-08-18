236c5d78ed3153598bb8580894fb22bb5ebbadb8
SpringHgui <740360381@qq.com>
2021-08-18 23:57:42 +0800

    api

---
+++ FastTunnel.Api/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using FastTunnel.Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastTunnel.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected ApiResponse ApiResponse = new ApiResponse();
+    }
+}
