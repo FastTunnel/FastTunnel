5cfe7dad7899c238149d97bb9e63f40d37d98230
Gui.H <springhgui@outlook.com>
2022-12-29 18:46:00 +0800

    1

--- FastTunnel.Server/Program.cs
+++ FastTunnel.Server/Program.cs
@@ -4,16 +4,23 @@
 //     https://github.com/FastTunnel/FastTunnel/edit/v2/LICENSE
 // Copyright (c) 2019 Gui.H
 
+using System;
 using System.Net.Http;
 using System.Net.Sockets;
+using System.Text;
 using FastTunnel.Api.Filters;
+using FastTunnel.Core.Config;
 using FastTunnel.Core.Extensions;
+using FastTunnel.Server.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using Serilog.Events;
 
@@ -63,6 +70,37 @@ public class Program
             // -------------------FastTunnel STEP1 OF 3------------------
             builder.Services.AddFastTunnelServer(builder.Configuration.GetSection("FastTunnel"));
             // -------------------FastTunnel STEP1 END-------------------
+
+            var Configuration = builder.Configuration;
+            var apioptions = Configuration.GetSection("FastTunnel").Get<DefaultServerConfig>();
+
+            builder.Services.AddAuthentication("Bearer").AddJwtBearer(delegate (JwtBearerOptions options)
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.FromSeconds(apioptions.Api.JWT.ClockSkew),
+                    ValidateIssuerSigningKey = true,
+                    ValidAudience = apioptions.Api.JWT.ValidAudience,
+                    ValidIssuer = apioptions.Api.JWT.ValidIssuer,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(apioptions.Api.JWT.IssuerSigningKey))
+                };
+                options.Events = new JwtBearerEvents
+                {
+                    OnChallenge = async delegate (JwtBearerChallengeContext context)
+                    {
+                        context.HandleResponse();
+                        context.Response.ContentType = "application/json;charset=utf-8";
+                        await context.Response.WriteAsJsonAsync(new
+                        {
+                            code = -1,
+                            message = context.Error ?? "未登录"
+                        });
+                    }
+                };
+            });
 
             builder.Host.UseWindowsService();
 
